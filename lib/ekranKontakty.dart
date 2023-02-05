f92b001e3457fe044a8d6fa4e5c9bb660461d675
Marcin <marcinmaciejasz@op.pl>
2023-02-05 21:00:38 +0100

    First version with mock-up main menu with 4 buttons and working map

---
+++ lib/ekranKontakty.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EkranKontakty extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Tutaj będzie zarządzenie kontaktami'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,78 @@
 import 'package:flutter/material.dart';
-
+import 'ekranKontakty.dart';
+import 'ekranSpotkania.dart';
+import 'ekranUstawienia.dart';
+import 'map.dart';
+import 'lake.dart';
+// Uncomment lines 3 and 6 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-
-var app = MaterialApp(
-    home: Text('Hello from flutter'),
-    );
-
-
-runApp(app);
+  // debugPaintSizeEnabled = true;
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
- 
 
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MediaQuery(
+      data: MediaQuery.of(context),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EkranKontakty(),
+                  ),
+                ),
+                child: Text('Zarządzaj kontaktami'),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EkranSpotkania(),
+                  ),
+                ),
+                child: Text('Zarządzaj spotkaniem'),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MapScreen(),
+                  ),
+                ),
+                child: Text('Map'),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Lake(),
+                  ),
+                ),
+                child: Text('Lake'),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EkranUstawienia(),
+                  ),
+                ),
+                child: Text('Ustawienia'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
